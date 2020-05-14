dd506725c498338a00ce5ddad38af0364a480c59
권석민 <killroad@gmail.com>
2020-05-14 16:26:17 +0900

    최초커밋

---
+++ src/main/java/com/ilovebambi/mytoyboot/MyToyBootApplication.java
@@ -0,0 +1,13 @@
+package com.ilovebambi.mytoyboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyToyBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyToyBootApplication.class, args);
+    }
+
+}
